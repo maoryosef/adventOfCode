479d844475d93008190637edb57cca43a89bcb7d
Maor Yosef <maory@wix.com>
2017-12-21 23:29:00 +0200

    initial commit

---
+++ puzzle19/puzzle19.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+const input = fs.readFileSync(`${__dirname}/prodInput.txt`, 'utf-8');
+const inputMatrix = [];
+
+input.split('\n').forEach(line => {
+    inputMatrix.push(line.split(''));
+    }
+)
+
+const entryPoint = inputMatrix[0].indexOf('|');
+let letters = '';
+let steps = 0;
+function traverse(arr, x, y, dir) {
+    let finish = false;
+    while (!finish) {
+        steps++;
+        switch(dir) {
+            case 'D': y += 1;
+            break;
+            case 'U': y -= 1;
+            break;
+            case 'L': x -= 1;
+            break;
+            case 'R': x += 1;
+            break;
+        }
+    
+        if ('+-|'.indexOf(arr[y][x]) === -1) {
+            if (arr[y][x] === ' ') {
+                return;
+            }
+    
+            letters += arr[y][x];
+        }
+    
+        if (arr[y][x] === '+') {
+            if (dir === 'D' || dir === 'U') {
+                if (arr[y][x-1] !== ' ') {
+                    dir = 'L';
+                } else if (arr[y][x+1] !== ' ') {
+                    dir = 'R';
+                }
+            } else {
+                if (arr[y+1] && arr[y+1][x] !== ' ') {
+                    dir = 'D'
+                } else {
+                    dir = 'U'
+                }
+            }
+        }
+    }
+}
+
+traverse(inputMatrix, entryPoint, 0, 'D');
+console.log(letters)
+console.log(steps)
---
+++ puzzle3/puzzle3.p1.js
@@ -0,0 +1,52 @@
+// const target = 289326;
+const target = 289326;
+
+let i = 1;
+let step = 0;
+let minTarget = 3;
+let radius = 1;
+let curX = 0;
+let curY = 0;
+let goLeft = false;
+let goRight = true;
+let goUp = false;
+let goDown = false;
+
+while (i < target) {
+    i += 1;
+
+    if (curX === radius && goRight) {
+        goRight = false;
+        goUp = true;
+    }
+
+    if (curX === -radius && goLeft) {
+        goLeft = false;
+        goDown = true;
+    }
+
+    if (curY === radius && goDown) {
+        goDown = false;
+        goRight = true;
+    }
+
+    if (curY === -radius && goUp) {
+        goUp = false;
+        goLeft = true;
+    }
+
+    curX += goLeft ? -1 : 0;
+    curX += goRight ? 1 : 0;
+    curY += goUp ? -1 : 0;
+    curY += goDown ? 1 : 0;
+
+    if (i === minTarget ** 2) {
+        radius++;
+        minTarget += 2;
+    }
+}
+
+i
+
+console.log(radius, minTarget, minTarget ** 2,  i - minTarget ** 2, (i - minTarget ** 2) / 5);
+console.log(curX, curY, Math.abs(curX) + Math.abs(curY));
